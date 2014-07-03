b0518a8b656c758bc3a5f50754b0e92e8c26160b
patmast3r <patmast3r@unseen.is>
2014-07-03 13:45:58 +0200

    using inheritance for sockets

--- handlers/SocketHandler.py
+++ handlers/SocketHandler.py
@@ -6,13 +6,15 @@ import tornado.gen
 import tornadoredis
 import tornado.websocket
 
-class LatestBlockSocket(tornado.websocket.WebSocketHandler):
-
+class LatestSocket(tornado.websocket.WebSocketHandler):
+    
+    channel = 'block_channel'
+    
     @tornado.gen.engine
     def open(self):
         self.client = tornadoredis.Client()
         self.client.connect()
-        yield tornado.gen.Task(self.client.subscribe, 'block_channel')
+        yield tornado.gen.Task(self.client.subscribe, self.channel)
         self.client.listen(self.on_message)
 
     def on_message(self, msg):
@@ -23,25 +25,11 @@ class LatestBlockSocket(tornado.websocket.WebSocketHandler):
         
     def on_close(self):
         if self.client.subscribed:
-            self.client.unsubscribe('block_channel')
+            self.client.unsubscribe(self.channel)
             self.client.disconnect()
 
-class LatestTxSocket(tornado.websocket.WebSocketHandler):
+class LatestBlockSocket(LatestSocket):
+    channel = 'block_channel'
 
-    @tornado.gen.engine
-    def open(self):
-        self.client = tornadoredis.Client()
-        self.client.connect()
-        yield tornado.gen.Task(self.client.subscribe, 'tx_channel')
-        self.client.listen(self.on_message)
-
-    def on_message(self, msg):
-        if msg.kind == 'message':
-            self.write_message(msg.body)
-        if msg.kind == 'disconnect':
-            self.close()
-        
-    def on_close(self):
-        if self.client.subscribed:
-            self.client.unsubscribe('tx_channel')
-            self.client.disconnect()
+class LatestTxSocket(LatestSocket):
+    channel = 'tx_channel'
